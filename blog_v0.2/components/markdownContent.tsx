f0fdebbe627d90539537862812c824a1fd648938
Kim-TaeHyeong <taehyeok02@gmail.com>
2024-03-02 09:28:37 +0000

    mobile issue fix

--- blog_v0.2/components/markdownContent.tsx
+++ blog_v0.2/components/markdownContent.tsx
@@ -1,5 +1,5 @@
 export default function MarkdownContent({postData} : PostProp) {
     return (        
-        <div className="markdown-body" dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+        <div className="markdown-body bg-white" dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
     )
 }
